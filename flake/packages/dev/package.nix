af17893141dc5664333febb40f588e0cb6a3d219
David Oleander <dta@alumni.cmu.edu>
2024-09-28 04:19:46 +0000

    combine devserver into dev

--- flake/packages/dev/package.nix
+++ flake/packages/dev/package.nix
@@ -1,5 +1,11 @@
-{ writeScriptBin, bash, watch }:
+{ bash, deno, elmPackages, watch, writeScriptBin }:
   writeScriptBin "dev" ''
-#! ${bash}/bin/bash
-${watch}/bin/watch --paths flake/packages/devserver backend frontend/elm.json frontend/src -- nix run .#devserver
-''
+    #! ${bash}/bin/bash
+    ${watch}/bin/watch --paths flake/packages/dev backend frontend/elm.json frontend/src -- nix run .#dev.server
+  '' // {
+    server = writeScriptBin "server" ''
+      #! ${bash}/bin/bash
+      (cd frontend && ${elmPackages.elm}/bin/elm make src/Main.elm --output=../build/frontend/index.html)
+      ${deno}/bin/deno run --allow-net --allow-read backend/src/main.ts
+    '';
+  }
--- flake/packages/devserver/package.nix
+++
@@ -1,6 +0,0 @@
-{ bash, stdenv, deno, elmPackages, writeScriptBin  }:
-  writeScriptBin "devserver" ''
-    #! ${bash}/bin/bash
-    (cd frontend && ${elmPackages.elm}/bin/elm make src/Main.elm --output=../build/frontend/index.html)
-    ${deno}/bin/deno run --allow-net --allow-read backend/src/main.ts
-  ''

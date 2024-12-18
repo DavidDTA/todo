99ad63d2bc4dedf789008a3a71f70f0393761c5b
David Oleander <dta@alumni.cmu.edu>
2024-12-18 11:44:42 +0000

    fixea

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,18 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
-function transact(operation) {
-  while (!operation()) {
+async function transact(operation) {
+  while (!await operation()) {
   }
 }
 
 async function getAllWaypoints(kv) {
   const waypoints = kv.list({"prefix": ["waypoints"]});
   const result = [];
-  for await (waypoint of waypoints) {
+  for await (const waypoint of waypoints) {
     result.push({
       "id": waypoint.key[1],
       "text": String(waypoint.value.text),
-      "complete": Boolean(waypoint.value.complete)
+      "completed": Boolean(waypoint.value.completed)
     });
   }
   return result;
@@ -27,31 +27,31 @@ Deno.serve(async (req) => {
   } else if (req.method == "GET" && route_api_waypoints) {
     const kv = await Deno.openKv();
     const waypoints = await getAllWaypoints(kv);
-    kv.close()
+    await kv.close()
     return new Response(JSON.stringify({"waypoints": waypoints}));
   } else if (req.method == "DELETE" && route_api_waypoints_id) {
     const id = route_api_waypoints_id.pathname.groups.id;
     const kv = await Deno.openKv();
-    kv.close()
     await kv.delete(["waypoints", id]);
+    await kv.close()
     return new Response("");
   } else if (req.method == "PATCH" && route_api_waypoints_id) {
     const id = route_api_waypoints_id.pathname.groups.id;
     const body = await req.json();
     const kv = await Deno.openKv();
     const key = ["waypoints", id]
-    transact(async () => {
+    await transact(async () => {
       const entry = await kv.get(key);
       const waypoint = entry.value || {};
       if ('text' in body) {
         waypoint.text = String(body.text);
       }
       if ('completed' in body) {
-        waypoint.completed = Boolean(body.complete);
+        waypoint.completed = Boolean(body.completed);
       }
-      return kv.atomic().check(entry).set(key, waypoint).commit().ok;
+      return (await kv.atomic().check(entry).set(key, waypoint).commit()).ok;
     });
-    kv.close()
+    await kv.close()
     return new Response("");
   }
   return new Response(null, { "status": 404 });

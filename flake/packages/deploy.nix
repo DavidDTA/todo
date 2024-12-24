eefb399d7019c31bc68150aa14b5a65c3095d49a
David Oleander <dta@alumni.cmu.edu>
2024-12-24 19:39:48 +0000

    add deploy

---
+++ flake/packages/deploy.nix
@@ -0,0 +1,12 @@
+{ bash, deno, elmPackages, git, writeScriptBin }:
+    writeScriptBin "deploy" ''
+      #! ${bash}/bin/bash
+      set -e
+      outdir="$(pwd)/build/deploy"
+      rm -r "''${outdir}" 2>/dev/null || true
+      mkdir -p "''${outdir}"
+      cp -r backend "''${outdir}/server"
+      (cd frontend && ${elmPackages.elm}/bin/elm make src/Main.elm --optimize --output=''${outdir}/server/index.html)
+      (cd "''${outdir}/server" && ${deno}/bin/deno run --allow-sys --allow-env --allow-read --allow-net jsr:@deno/deployctl deploy --save-dev "''${@}")
+      ${git}/bin/git diff backend/deno.json build/deploy/server/deno.json
+    ''

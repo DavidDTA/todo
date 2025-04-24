859920d20c65539132fbf3d63483fc21767f2939
David Oleander <dta@alumni.cmu.edu>
2025-04-24 05:44:57 -0400

    Fix deploy script permissions

--- flake/packages/deploy.nix
+++ flake/packages/deploy.nix
@@ -17,6 +17,6 @@
       (cd elm && ${elmPackages.elm}/bin/elm make src/Frontend/Main.elm --optimize --output="''${outdir}/server/index-authenticated.html")
       (cd elm && ${elmPackages.elm}/bin/elm make src/Backend/Main.elm --optimize --output="''${outdir}/server/src/elm/main.js")
       ${elm-to-esm}/bin/elm-to-esm "''${outdir}/server/src/elm/main.js"
-      (cd "''${outdir}/server" && ${deno}/bin/deno run --allow-sys --allow-env --allow-read --allow-write=deno.json --allow-write=~/.deno --allow-net jsr:@deno/deployctl deploy --save-config --project "''${project}")
+      (cd "''${outdir}/server" && ${deno}/bin/deno run --allow-sys --allow-env --allow-read --allow-write=deno.json --allow-write=~/.deno/deployctl --allow-net jsr:@deno/deployctl deploy --save-config --project "''${project}")
       ${git}/bin/git diff --no-index -- backend/deno.json ''${outdir}/server/deno.json
     ''

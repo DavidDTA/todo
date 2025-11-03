0470eb13bcee2329c3efafc347a42e76ce6c0d4d
David Oleander <dta@alumni.cmu.edu>
2025-11-03 15:55:31 -0500

    Specify keyboard viewport behavior

--- flake/packages/build.nix
+++ flake/packages/build.nix
@@ -10,7 +10,7 @@
       <html>
       <head>
       <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1,interactive-widget=resizes-content">
       <title>Working on it!</title>
       ''${devtools}
       <script src="/-/app.js"></script>

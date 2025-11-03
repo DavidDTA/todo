374f0625e94fddc07e607e1004c2f9523bf17440
David Oleander <dta@alumni.cmu.edu>
2025-11-03 15:36:11 -0500

    Add viewport meta

--- flake/packages/build.nix
+++ flake/packages/build.nix
@@ -10,6 +10,7 @@
       <html>
       <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Working on it!</title>
       ''${devtools}
       <script src="/-/app.js"></script>

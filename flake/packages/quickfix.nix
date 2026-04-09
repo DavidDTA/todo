8871cbe1329e529ccc26a65eecea9fb57134082f
David Oleander <dta@alumni.cmu.edu>
2026-04-08 21:02:33 -0400

    Improve quickfix --no-wire workaround

--- flake/packages/quickfix.nix
+++ flake/packages/quickfix.nix
@@ -3,7 +3,7 @@
     #! ${bash}/bin/bash
     set -eEuo pipefail
     (cd elm && {
-      ${elmPackages.lamdera}/bin/lamdera make --no-wire src/Frontend/Login.elm src/Frontend/Main.elm src/Backend/Main.elm --output=/dev/null >/dev/null 2>/dev/null || true
+      ${elmPackages.lamdera}/bin/lamdera make --no-wire src/Frontend/Login.elm src/Frontend/Main.elm src/Backend/Main.elm --report=json --output=/dev/null >/dev/null || true
       ${elmPackages.lamdera}/bin/lamdera make src/Frontend/Login.elm --report=json --output=/dev/null >/dev/null
       ${elmPackages.lamdera}/bin/lamdera make src/Frontend/Main.elm --report=json --output=/dev/null >/dev/null
       ${elmPackages.lamdera}/bin/lamdera make src/Backend/Main.elm --report=json --output=/dev/null >/dev/null

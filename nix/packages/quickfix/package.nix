eac9d3eeb13d302ffc963f17ad709d05026e34e9
David Oleander <dta@alumni.cmu.edu>
2026-07-07 23:51:46 -0400

    Improve quickfix

--- nix/packages/quickfix/package.nix
+++ nix/packages/quickfix/package.nix
@@ -1,42 +1,55 @@
-{ bash, elmPackages, jq, writeScriptBin }:
-  writeScriptBin "quickfix" ''
-    #! ${bash}/bin/bash
-    set -eEuo pipefail
-    (cd elm && {
-      ${elmPackages.lamdera}/bin/lamdera make --no-wire src/Frontend/Login.elm src/Frontend/Main.elm src/Backend/Main.elm --report=json --output=/dev/null >/dev/null || true
-      ${elmPackages.lamdera}/bin/lamdera make src/Frontend/Login.elm --report=json --output=/dev/null >/dev/null
-      ${elmPackages.lamdera}/bin/lamdera make src/Frontend/Main.elm --report=json --output=/dev/null >/dev/null
-      ${elmPackages.lamdera}/bin/lamdera make src/Backend/Main.elm --report=json --output=/dev/null >/dev/null
-    }) 2>&1 >/dev/null |
-      ${jq}/bin/jq --raw-output '
-        .errors[] |
-        .path as $path |
-        .name as $module |
-        .problems[] |
-        $path +
-        ":" +
-        $module +
-        ":" +
-        (.region.start.line | tostring) +
-        ":" +
-        (.region.start.column | tostring) +
-        ":" +
-        (.region.end.line | tostring) +
-        ":" +
-        (.region.end.column | tostring) +
-        ":" +
-        .title +
-        "\n" +
-        (
-          .message |
-          map(
-            if type == "string" then .
-            else .string
-            end
-          ) |
-          join("")
-        )
+{ coreutils, elmPackages, jq, writeShellApplication }:
+  writeShellApplication {
+    name = "quickfix";
+    inheritPath = false;
+    runtimeInputs = [
+      coreutils
+      elmPackages.lamdera
+      jq
+    ];
+    text = ''
+      pushd elm >/dev/null
+      regular_exit_code=0
+      regular_output="$(lamdera make src/Frontend/Login.elm src/Frontend/Main.elm src/Backend/Main.elm --report=json --output=/dev/null 3>&1 >/dev/null 2>&3)" || regular_exit_code="$?"
+      no_wire_exit_code=0
+      no_wire_output="$(lamdera make --no-wire src/Frontend/Login.elm src/Frontend/Main.elm src/Backend/Main.elm --report=json --output=/dev/null 3>&1 >/dev/null 2>&3)" || no_wire_exit_code="$?"
+      popd >/dev/null
+      echo "$no_wire_output" "$regular_output" | jq --slurp --raw-output '
+        [
+          .[] |
+          .errors[] |
+          .path as $path |
+          .name as $module |
+          .problems[] |
+          $path +
+          ":" +
+          $module +
+          ":" +
+          (.region.start.line | tostring) +
+          ":" +
+          (.region.start.column | tostring) +
+          ":" +
+          (.region.end.line | tostring) +
+          ":" +
+          (.region.end.column | tostring) +
+          ":" +
+          .title +
+          "\n" +
+          (
+            .message |
+            map(
+              if type == "string" then .
+              else .string
+              end
+            ) |
+            join("")
+          )
+        ] |
+        sort_by(split("\n") | .[1] | contains("`w3_")) |
+        .[]
       '
-    mkdir -p build
-    touch build/touchstone
-  ''
+      mkdir -p build
+      touch build/touchstone
+      [ "$no_wire_exit_code" = "0" ] && [ "$regular_exit_code" = "0" ]
+    '';
+  }
